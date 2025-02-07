6ab5880863f81c638151e231805bfa7a93bb81ea
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-02-08 00:01:08 +0530

    Completely done.

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#button-reset");
-
-/**
- * PlayerX
- * PlayerO
- */
+let utility = document.querySelector(".utility");
+let newGame = document.querySelector("#newGame");
+let closemodel = document.querySelector("#close-model");
 
 let turnO = true;
 
+/* PlayerX
+   PlayerO  */
+
 const winningPattern = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontally
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertically
+    [0, 4, 8], [2, 4, 6] //diagonally
 ];
 
+//resetGame
+const resetGame = () => {
+    document.querySelectorAll(".box").forEach(box => {
+        document.getElementById("winnerModel").style.display = "none";
+        document.querySelector(".container").classList.remove("blur");
+        box.textContent = " ";
+        turnO = true;
+    });
+};
+
 boxes.forEach((box) => {
-    box.addEventListener("click", () =>{
+    box.addEventListener("click", () => {
         console.log("Box was hitted!");
-        if(turnO) {
+        if (turnO) {
             box.innerText = "O";
             turnO = false;
         }
@@ -30,24 +36,45 @@ boxes.forEach((box) => {
             box.innerText = "X";
             turnO = true;
         }
-        box.disabled = true;
-
-        checkWinner();
         
+        checkWinner();
+
     });
 });
 
+
+function DisplayWinner(winner) {
+    document.getElementById("winnerText").textContent = `Congratulation, player ${winner} Won!`;
+    document.getElementById("winnerModel").style.display = "block";
+    document.querySelector(".container").classList.add("blur");
+};
+
+//close model
+function closeModel() {
+    const modal = document.getElementById("winnerModel").style.display = "none";
+    document.querySelector(".container").classList.remove("blur");
+    resetGame();
+}
+
 const checkWinner = () => {
-    for(let pattern of winningPattern) {
+    for (let pattern of winningPattern) {
         let valPattern1 = boxes[pattern[0]].innerText;
         let valPattern2 = boxes[pattern[1]].innerText;
         let valPattern3 = boxes[pattern[2]].innerText;
 
-        if(valPattern1 != "" && valPattern2 != "" && valPattern3 != "") {
-            if(valPattern1 === valPattern2 && valPattern2 === valPattern3) {
-                
-                console.log("Winner");
+        if (valPattern1 != "" && valPattern2 != "" && valPattern3 != "") {
+            if (valPattern1 === valPattern2 && valPattern2 === valPattern3) {
+                DisplayWinner(valPattern1);
             }
         }
     }
 };
+
+//clodeModal
+closemodel.addEventListener("click", resetGame);
+
+//newGame
+newGame.addEventListener("click", resetGame);
+
+//resetGame
+resetBtn.addEventListener("click", resetGame);
